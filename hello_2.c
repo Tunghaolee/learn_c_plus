f0d8ea82da8b705019c98054c266972e078afec5
TungHaoLee <Tunghaolee@users.noreply.github.com>
2022-11-19 11:54:58 +0800

    Create hello_2.c

---
+++ hello_2.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main()
+{
+    printf("Hello World\n");
+    printf("my height is %d m %d cm\n",1,80);
+    printf("this building is %5d cm\n",800);
+    printf("my height is %10d cm\n",7890);
+    printf("my height is %10d cm\n",67890);
+    printf("my height is %10d cm\n",567890);
+    printf("my height is %10d cm\n",1234567890);
+    printf("pi=  %2.5f\n",3.14159265359);
+    return 0;
+}
